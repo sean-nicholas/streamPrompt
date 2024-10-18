372a33061a52cafb6c78e867a9289bcbf379dcc5
Sean Nicholas Dieterle <dieterle@sodefa.de>
2024-10-18 21:31:20 +0200

    ✨ edge

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,9 +1,7 @@
-import { MainTop } from '@/components/layout/MainTop'
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <MainTop />
+      {/* <MainTop /> */}
       <hr />
       <div className="container flex flex-col gap-8 py-8 flex-1">
         {children}
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,6 @@
 import { PartyButton } from '@/components/demo/PartyButton'
-import { Readme } from '@/components/demo/Readme'
-import { Button } from '@/components/ui/button'
-import { ExternalLink } from 'lucide-react'
-import Link from 'next/link'
 
-const runtime = 'edge'
+export const runtime = 'edge'
 
 export default async function Page() {
   return (
@@ -13,17 +9,7 @@ export default async function Page() {
         <h1 className="text-2xl lg:text-6xl">🎉 Welcome to the Party 🥳</h1>
         <div className="flex flex-col gap-2">
           <PartyButton />
-          <Link
-            href="https://github.com/new?template_name=party-starter&template_owner=rechenberger"
-            target="_blank"
-          >
-            <Button variant={'outline'}>
-              Use this template
-              <ExternalLink className="w-4 h-4 ml-1" />
-            </Button>
-          </Link>
         </div>
-        <Readme />
       </div>
     </>
   )
