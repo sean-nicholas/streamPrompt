6353a323e01ddd780c608d5048252138664473c1
Sean Nicholas Dieterle <dieterle@sodefa.de>
2024-10-19 18:23:40 +0200

    ⚗️ race

--- src/components/demo/PartyButton.tsx
+++ src/components/demo/PartyButton.tsx
@@ -1,3 +1,4 @@
+import { redisSubscribe } from '@/lib/redis'
 import { superAction } from '@/super-action/action/createSuperAction'
 import { ActionButton } from '@/super-action/button/ActionButton'
 import { PartyPopper } from 'lucide-react'
@@ -26,57 +27,86 @@ export const PartyButton = () => {
               // content: <div className="flex gap-2">hi</div>,
             })
 
-            let count = 0
-            while (true) {
-              await new Promise((resolve) => setTimeout(resolve, 1_000))
-              const message = `Still there. Iteration ${++count}. Run for ${
-                (new Date().getTime() - startedAt.getTime()) / 1000
-              } seconds`
-              console.log(message)
-              streamToast({
-                title: message,
-              })
-            }
-
-            // const res = await redisSubscribe({ key: 'party' })
-            // if (!res.ok || !res.body) {
-            //   console.error('Failed to subscribe to Redis')
-            //   return
-            // }
-            // const reader = res.body.getReader()
-            // let message = ''
-            // let data = null
+            // let count = 0
             // while (true) {
-            //   const { done, value } = await reader.read()
-            //   const text = new TextDecoder().decode(value)
-            //   message += text
-            //   const parts = message.split('\n')
-            //   console.log('parts', parts)
-            //   const dataPart = parts.find((part) =>
-            //     part.includes('data: message,party,'),
-            //   )
-            //   console.log('dataPart', dataPart)
-
-            //   if (dataPart) {
-            //     try {
-            //       const jsonString = dataPart.replace(
-            //         'data: message,party,',
-            //         '',
-            //       )
-            //       console.log('jsonString', jsonString)
-            //       const finalMessage = JSON.parse(jsonString)
-            //       console.log('finalMessage', finalMessage)
-            //       data = finalMessage.data
-            //       break
-            //     } catch (error) {}
-            //   }
+            //   await new Promise((resolve) => setTimeout(resolve, 1_000))
+            //   const message = `Still there. Iteration ${++count}. Run for ${
+            //     (new Date().getTime() - startedAt.getTime()) / 1000
+            //   } seconds`
+            //   console.log(message)
+            //   streamToast({
+            //     title: message,
+            //   })
             // }
-            // clearInterval(intervalId)
-            // reader.cancel()
 
-            // streamToast({
-            //   title: `You said: ${data}`,
-            // })
+            const res = await redisSubscribe({ key: 'party' })
+            if (!res.ok || !res.body) {
+              console.error('Failed to subscribe to Redis')
+              return
+            }
+            const reader = res.body.getReader()
+            let message = ''
+            let data = null
+            let count = 0
+            let readerPromise: Promise<{
+              type: 'reader'
+              result: ReadableStreamReadResult<Uint8Array>
+            }> | null = null
+            while (true) {
+              if (!readerPromise) {
+                readerPromise = reader.read().then((result) => ({
+                  type: 'reader' as const,
+                  result,
+                }))
+              }
+
+              const timeoutPromise = new Promise<{ type: 'timeout' }>(
+                (resolve) =>
+                  setTimeout(() => resolve({ type: 'timeout' }), 5000),
+              )
+              const result = await Promise.race([readerPromise, timeoutPromise])
+              if (result.type === 'timeout') {
+                const message = `Still there. Iteration ${++count}. Run for ${
+                  (new Date().getTime() - startedAt.getTime()) / 1000
+                } seconds`
+                console.log(message)
+                streamToast({
+                  title: message,
+                })
+              }
+
+              if (result.type === 'reader') {
+                const { done, value } = result.result
+                readerPromise = null
+                const text = new TextDecoder().decode(value)
+                message += text
+                const parts = message.split('\n')
+                console.log('parts', parts)
+                const dataPart = parts.find((part) =>
+                  part.includes('data: message,party,'),
+                )
+                console.log('dataPart', dataPart)
+
+                if (dataPart) {
+                  try {
+                    const jsonString = dataPart.replace(
+                      'data: message,party,',
+                      '',
+                    )
+                    console.log('jsonString', jsonString)
+                    const finalMessage = JSON.parse(jsonString)
+                    console.log('finalMessage', finalMessage)
+                    data = finalMessage.data
+                    break
+                  } catch (error) {}
+                }
+              }
+            }
+            reader.cancel()
+
+            streamToast({
+              title: `You said: ${data}`,
+            })
 
             // console.log(await res.text())
           })
